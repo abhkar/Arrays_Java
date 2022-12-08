fa0f96d5e6546eb212a64f7b50f8995d5dd8ed17
abhishekmalakar <malakar.abh@gmail.com>
2022-12-08 22:54:52 +0530

    Changes made

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,20 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+/*
+       int arr[]=new int[3];
+
+       arr[0]=2;
+       arr[1]=3;
+       arr[2]=4;
+*/
+       int arr1[]={5,6,7,8,9};
+
+       for(int i:arr1){
+           System.out.println(arr1[i]);
+       }
+
+
     }
 }
